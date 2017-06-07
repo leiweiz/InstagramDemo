b31c8f25771e803bfd3aecd75af52910d1cc0c9e
leiweiz <zhengleiwei@hotmail.com>
2017-06-07 00:11:46 -0400

    add htmlwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
 
+
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
 class App extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <div>Hi</div>;
+        return (
+            <MuiThemeProvider>
+                <MyAwesomeReactComponent />
+            </MuiThemeProvider>
+        )
     }
 }
 
